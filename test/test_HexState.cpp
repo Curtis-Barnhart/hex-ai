66e6b6e21b5cda57d381b6693b4d3967515667a8
Curtis Barnhart <cbarnhart@westmont.edu>
2024-06-10 09:59:46 -0700

    finished remaking test

--- test/test_HexState.cpp
+++ test/test_HexState.cpp
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+#include <utility>
 
 #include "hex-ai/GameState/HexState.hpp"
 #include "hex-ai/GameSolve/DeepNodeSolve.hpp"
@@ -12,7 +13,11 @@ using GameState::HexState::PLAYERS::PLAYER_NONE;
 * Definitions                                    *
 *************************************************/
 
-TEST(HexState_Definitions)
+TEST(HexState_Definitions, PLAYERSValue) {
+    EXPECT_EQ(PLAYER_ONE, 0);
+    EXPECT_EQ(PLAYER_TWO, 1);
+    EXPECT_EQ(PLAYER_NONE, 2);
+}
 
 /*************************************************
 * Trivials                                       *
@@ -58,18 +63,139 @@ TEST(HexState_Movement, TurnCount) {
     GameSolve::hex_rand_moves(state, 5);
     ASSERT_EQ(state.who_won(), PLAYER_NONE)
         << "No one can win in 5 turns";
-    EXPECT_EQ(state.whose_turn(), 1);
+    EXPECT_EQ(state.whose_turn(), 1)
+        << "It should be player 2's turn after 5 moves";
+    EXPECT_EQ(state.get_actions()->size(), 116)
+        << "There should be 116 moves after 5 have been taken";
+
+    GameSolve::hex_rand_moves(state, 5);
+    ASSERT_EQ(state.who_won(), PLAYER_NONE)
+        << "No one can win in 10 turns";
+    EXPECT_EQ(state.whose_turn(), 0)
+        << "It should be player 1's turn after 10 moves";
+    EXPECT_EQ(state.get_actions()->size(), 111)
+        << "There should be 111 moves after 10 have been taken";
+
+    GameSolve::hex_rand_moves(state, 5);
+    ASSERT_EQ(state.who_won(), PLAYER_NONE)
+        << "No one can win in 15 turns";
+    EXPECT_EQ(state.whose_turn(), 1)
+        << "It should be player 2's turn after 15 moves";
+    EXPECT_EQ(state.get_actions()->size(), 106)
+        << "There should be 106 moves after 15 have been taken";
+}
+
+TEST(HexState_Movement, WinMoveCount) {
+    HexState state;
+
+    for (unsigned char y = 0; y < 11; y++) {
+        state.succeed_in_place({0, y, PLAYER_ONE});
+    }
+
+    EXPECT_EQ(state.get_actions()->size(), 0)
+        << "There should be 0 available moves after a game has been won";
 }
 
 /*************************************************
 * WinCondition                                   *
 *************************************************/
 
+TEST(HexState_WinCondition, PlayerOneWin) {
+    HexState state;
+
+    for (unsigned char y = 0; y < 11; y++) {
+        state.succeed_in_place({0, y, PLAYER_ONE});
+    }
+
+    EXPECT_EQ(state.who_won(), PLAYER_ONE)
+        << "Player 1 should have won this game";
+}
+
+TEST(HexState_WinCondition, PlayerOneWinFalse) {
+    HexState state;
+
+    for (unsigned char x = 0; x < 11; x++) {
+        state.succeed_in_place({x, 0, PLAYER_NONE});
+    }
+
+    EXPECT_EQ(state.who_won(), PLAYER_NONE)
+        << "No one should win as Player 1 has gone the wrong direction";
+}
+
+TEST(HexState_WinCondition, PlayerTwoWin) {
+    HexState state;
+
+    for (unsigned char x = 0; x < 11; x++) {
+        state.succeed_in_place({x, 0, PLAYER_TWO});
+    }
+
+    EXPECT_EQ(state.who_won(), PLAYER_TWO)
+        << "Player 2 should have won";
+}
+
+TEST(HexState_WinCondition, PlayerTwoWinFalse) {
+    HexState state;
+
+    for (unsigned char y = 0; y < 11; y++) {
+        state.succeed_in_place({0, y, PLAYER_TWO});
+    }
+
+    EXPECT_EQ(state.who_won(), PLAYER_NONE)
+        << "No one should win as Player 2 has gone the wrong direction";
+}
+
 /*************************************************
 * ConstructionAssignment                         *
 *************************************************/
 
+TEST(HexState_CtorAndAsnmt, CopyMoveCtor) {
+    HexState state;
+    GameSolve::hex_rand_moves(state, 10);
+
+    HexState s1(state);
+    ASSERT_EQ(s1, state)
+        << "s1 was copy constructed from state and should be equal";
+    HexState s2(std::move(state));
+    EXPECT_EQ(s2, s1)
+        << "s2 was move constructed from state and should be equal to s1";
+}
+
+TEST(HexState_CtorAndAsnmt, CopyMoveAsnmt) {
+    HexState state, s1, s2;
+    GameSolve::hex_rand_moves(state, 10);
+
+    s1 = state;
+    ASSERT_EQ(s1, state)
+        << "s1 was copy assigned from state and should be equal";
+    s2 = std::move(state);
+    EXPECT_EQ(s2, s1)
+        << "s2 was move assigned from state and should be equal to s1";
+}
+
 /*************************************************
 * Equality                                       *
 *************************************************/
+
+TEST(HexState_Equality, EqualStates) {
+    HexState s1, s2;
+    s1.succeed_in_place({0, 0, PLAYER_ONE});
+    s2.succeed_in_place({0, 0, PLAYER_ONE});
+    s1.succeed_in_place({1, 1, PLAYER_TWO});
+    s2.succeed_in_place({1, 1, PLAYER_TWO});
+    s1.succeed_in_place({2, 2, PLAYER_ONE});
+    s2.succeed_in_place({2, 2, PLAYER_ONE});
+
+    EXPECT_EQ(s1, s2)
+        << "s1 and s2 have the same moves applied and should be equal";
+}
+
+TEST(HexState_Equality, InequalStates) {
+    HexState s1, s2;
+    s1.succeed_in_place({0, 0, PLAYER_ONE});
+    s1.succeed_in_place({1, 1, PLAYER_TWO});
+    s1.succeed_in_place({2, 2, PLAYER_ONE});
+
+    EXPECT_NE(s1, s2)
+        << "s1 and s2 have different moves applied and should be inequal";
+}
 
