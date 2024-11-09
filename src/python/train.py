f60026fb63a6c67c2de1c9fa6e447cddc771fc38
Curtis Barnhart <cbarnhart@westmont.edu>
2024-11-09 13:00:55 -0800

    train.py now takes validation data and saves checkpoints

--- src/python/train.py
+++ src/python/train.py
@@ -13,8 +13,10 @@ def generate_model():
     print("Generating new model")
     model = keras.models.Sequential([
         keras.layers.Input((363,)),
-        keras.layers.Dense(800, activation='relu'),
-        keras.layers.Dense(800, activation='relu'),
+        keras.layers.Dense(1600, activation='relu'),
+        # keras.layers.Dropout(0.1),
+        keras.layers.Dense(1600, activation='relu'),
+        # keras.layers.Dropout(0.1),
         keras.layers.Dense(2)
     ])
 
@@ -29,15 +31,35 @@ def generate_model():
 
 if __name__ == "__main__":
     model = generate_model()
-    dataset = read_from_tfrecords(sys.argv[1:])
+    dataset = read_from_tfrecords(sys.argv[1:-2])
+    validation = read_from_tfrecords(sys.argv[-2])
+    save_model = sys.argv[-1]
     infinite_dataset = dataset.repeat()
+    infinite_validation = validation.repeat()
 
     # Find size of dataset so we know how big to make steps_per_epoch
     dataset_size = sum(1 for _ in dataset)
-    print("Loaded in {:d} records.".format(dataset_size))
+    print("Loaded in {:d} training records.".format(dataset_size))
+    validation_size = sum(1 for _ in validation)
+    print("Loaded in {:d} validation records.".format(validation_size))
 
-    batch_size: int = 128
+    batch_size: int = 512
     steps: int = dataset_size // batch_size
+    validation_steps: int = validation_size // batch_size
 
-    model.fit(infinite_dataset, batch_size=batch_size, steps_per_epoch=steps, epochs=3)
+    model.fit(
+        infinite_dataset,
+        batch_size=batch_size,
+        steps_per_epoch=steps,
+        validation_data=infinite_validation,
+        validation_batch_size=batch_size,
+        validation_steps=validation_steps,
+        epochs=240,
+        callbacks=[
+            keras.callbacks.ModelCheckpoint(
+                filepath=save_model,
+                save_best_only=True
+            )
+        ]
+    )
 
