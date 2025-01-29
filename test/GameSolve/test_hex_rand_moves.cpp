33b6cddd5efb50495440f51c012bc1b7515fea28
Curtis Barnhart <cbarnhart@westmont.edu>
2025-01-29 09:15:26 -0800

    bumped date

--- test/GameSolve/test_hex_rand_moves.cpp
+++ test/GameSolve/test_hex_rand_moves.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 Curtis Barnhart (cbarnhart@westmont.edu)
+ * Copyright 2025 Curtis Barnhart (cbarnhart@westmont.edu)
  * This file is part of hex-ai.
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
